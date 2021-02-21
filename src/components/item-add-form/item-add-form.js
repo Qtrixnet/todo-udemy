6a10d0e19182b8ab8a663bc080b58269def2aa4a
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-02-21 22:20:57 +0600

    feat: добавить поиск и фильтры

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -6,34 +6,37 @@ export default class ItemAddForm extends Component {
 
   state = {
     label: ''
-  }
+  };
 
   onLabelChange = (e) => {
     this.setState({
       label: e.target.value
-    });
+    })
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.onItemAdded(this.state.label);
-  }
+    const { label } = this.state;
+    this.setState({ label: '' });
+    const cb = this.props.onItemAdded || (() => {});
+    cb(label);
+  };
 
   render() {
     return (
-      <form className="item-add-form d-flex"
-            onSubmit={this.onSubmit}>
-        <input
-          type="text"
-          className="form-control"
-          onChange={this.onLabelChange}
-          placeholder="What needs to be done"/>
-        <button
-          className="btn btn-outline-secondary"
-        >
-          Add Item
-        </button>
+      <form
+        className="bottom-panel d-flex"
+        onSubmit={this.onSubmit}>
+
+        <input type="text"
+               className="form-control new-todo-label"
+               value={this.state.label}
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done?" />
+
+        <button type="submit"
+                className="btn btn-outline-secondary">Add</button>
       </form>
-    )
+    );
   }
 }
